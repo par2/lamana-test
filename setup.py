e03b4a642a573ee0b20e50133340e9e5ff2da564
par2 <par2.get@gmail.com>
2015-11-27 22:52:21 -0500

    Add controls_LT folder

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     author_email='par2.get@gmail.com',
     url='https://github.com/par2/lamana',             # use the URL to the github repo
     download_url='https://github.com/par2/lamana/tarball/0.4.6',
-    packages=['lamana', 'lamana.models', 'lamana.utils', 'lamana.tests'],
+    packages=['lamana', 'lamana.models', 'lamana.utils', 'lamana.tests',
+              'lamana.tests.controls_LT'],
     keywords=['laminate analysis', 'visualization'],
     #install_requires=['matplotlib', 'pandas', 'numpy'],
     classifiers=[
